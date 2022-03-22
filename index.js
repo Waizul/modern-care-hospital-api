ac1ca6aadabf8862b3d8fe1c69a31a37471f4281
Waizul Haque <waizul@gmail.com>
2022-03-22 17:05:21 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express =  require('express')
 const app = express()
 const cors = require('cors')
-const mongodb = require('mongodb')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
+//middleware
 app.use(cors())
 app.use(express.json())
+
+//mongodb connection
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      
+      const DB = client.db('mchDB')
+
+      
+    } finally {
+      // Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
 
 app.get('/',(req,res)=>{
     res.send('mdc')
